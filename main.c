0734cd38163d5aa0680621f59b1e00f82dc3d467
AuroraYukihime <sacrain@vt.edu>
2019-05-25 00:33:52 -0400

    Initial commit - contains necessary files for build

---
+++ main.c
@@ -0,0 +1,12 @@
+// System Includes
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <ti/grlib/grlib.h>
+#include "LcdDriver/Crystalfontz128x128_ST7735.h"
+
+int main(void)
+{
+    while(1)
+    {
+
+    }
+}
